3be406e7e3e03a44b8c51164cd70a1dd063b9cf3
yyysolhhh <solbaram37@gmail.com>
2024-03-23 22:07:02 +0900

    Fix: fix mypy error, add environment to docker-compose.yml

--- app/routers/question.py
+++ app/routers/question.py
@@ -1,16 +1,24 @@
 from fastapi import APIRouter, Request, Response
 from fastapi.responses import HTMLResponse, RedirectResponse
 
+# import templates
+from fastapi.templating import Jinja2Templates
+from starlette.templating import _TemplateResponse
+
 from app.routers.welcome import templates
 from app.services.question import get_questions
+
+# from app.services.question import get_questions
+#
+# templates = Jinja2Templates(directory="app/templates")
 
 router = APIRouter()
 
 
 @router.get("", response_class=HTMLResponse)
-async def answer(request: Request) -> Response:
+async def answer(request: Request) -> Response | _TemplateResponse:
     participant_id = request.cookies.get("participant_id")
     if not participant_id:
         return RedirectResponse("/", status_code=302)
     questions_list = await get_questions()
-    return templates.TemplateResponse(request=request, name="question.html", questions=questions_list)  # type: ignore
+    return templates.TemplateResponse(request=request, name="question.html", context={"question_list": questions_list})
--- app/services/question.py
+++ app/services/question.py
@@ -5,8 +5,6 @@ from app.models.question import Question
 
 # async def get_questions(conn: BaseDBAsyncClient) -> int:
 async def get_questions() -> list[str]:
-    # sql = "INSERT INTO participants(name, age, gender) VALUES (%s, %s, %s)"
-    # participant_id: int = await conn.execute_insert(sql, [data.name, data.age, data.gender])
     questions: list[Question] = await Question.get_all_questions()
     questions_list: list[str] = [question.content for question in questions]
     return questions_list
