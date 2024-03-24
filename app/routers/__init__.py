79f3a75c44ee5a344986b43f7b4bcc5ff59f2e26
yyysolhhh <solbaram37@gmail.com>
2024-03-25 04:52:32 +0900

    Docs: add api doc to README.md

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -3,7 +3,7 @@ from app.routers import answer, participant, question, result, welcome
 from fastapi import APIRouter
 
 api_router = APIRouter()
-api_router.include_router(welcome.router, prefix="")
+api_router.include_router(welcome.router, prefix="", tags=["Welcome"])
 api_router.include_router(participant.router, prefix="/participants", tags=["Participant"])
 api_router.include_router(question.router, prefix="/questions", tags=["Question"])
 api_router.include_router(answer.router, prefix="/answers", tags=["Answer"])
